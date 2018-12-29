337a980a2548d75379823ca7756850907273fda2
evertonpendragon <everton.de.rezende@gmail.com>
2018-12-29 21:33:01 -0200

    Atualização do dicionário

---
+++ WarhammerCatalogueTradutor.py
@@ -0,0 +1,185 @@
+# encoding: utf-8
+# encoding: iso-8859-1
+# encoding: win-1252
+# encoding=utf8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+import os
+import json
+import zipfile as zip
+from shutil import copyfile
+from bs4 import BeautifulSoup
+from googletrans import Translator
+translator = Translator()
+########################################################################################################################
+# gera os arquivos catz prontos para serem importados no Battlescribe
+########################################################################################################################
+
+def compactCat(destPath):
+    print '\n\n\n\n',  'Compactando arquivo'
+    os.chdir(destPath)
+    destCatFiles = os.listdir('.')
+    destCatFiles = filter(lambda x: x[-1] != 'z', destCatFiles)
+    for f in destCatFiles:
+        print f
+        compression = zip.ZIP_DEFLATED
+        with zip.ZipFile(f+'z', mode='w') as zip_ref:
+            zip_ref.write( f, compress_type=compression)
+            zip_ref.close()
+        os.remove(f)
+
+
+originPath = "./wh40k-master/"
+destPath = "./wh40k_PTBR-master/"
+
+if not os.path.exists(destPath):
+    os.makedirs(destPath)
+
+for f in os.listdir(originPath):
+    originFileFullPath = originPath+f
+    destFileFullPath   = destPath+f
+
+    if os.path.splitext(originFileFullPath)[1] in [".catz",'.gstz']:
+        with zip.ZipFile(f, 'r') as zip_ref:
+            zip_ref.extractall(destPath)
+    elif os.path.splitext(originFileFullPath)[1] in ['.cat', '.gst']:
+        copyfile(originFileFullPath, destFileFullPath)
+    else:
+        continue
+
+
+#catz = filter(lambda x: 'Wolves.cat' in x, catz) #filtra somente um arquivo dos sparce wolves
+
+with open('dicionario.json', "r") as file:
+    dicionario = json.loads(file.read())
+    file.close()
+
+catz = os.listdir(destPath)
+for cfile in catz:
+    ccatFile = destPath + cfile
+    with open(ccatFile, "r") as file:
+        catFile = file.read()
+        file.close()
+    cat = BeautifulSoup(catFile, "xml")
+    #print catFile
+
+
+    if 1==1: #filtro para debbug
+        # TRADUZ ARQUIVO CAT
+        for catalogues in cat.find_all("catalogue"):
+            for catalogue in catalogues.children:
+                # print catalogue.name, type(catalogue)
+                if catalogue.name <> None:
+                    print 'n1', catalogue.name
+                    if catalogue.name in ["sharedrules", "sharedprofiles", "selectionentries", "sharedselectionentries",
+                                          "sharedselectionentrygroups","sharedRules","sharedProfiles","sharedSelectionEntryGroups",
+                                          "sharedSelectionEntries","selectionEntries"]:
+                        for catElement in catalogue.children:
+                            if catElement.name <> None:
+                                for ruleTag in catElement.find_all("description"):
+                                    rule = ruleTag.get_text()  #
+                                    # print rule
+                                    if rule in dicionario.keys() and len(rule) > 1:
+                                        rulePT = dicionario[rule]
+                                        print "Traduzindo>>>\n", rule, "\n", rulePT
+                                        #print type(cat)
+                                        try:
+                                            #cat.find(text=rule).replaceWith(rulePT)
+                                            ruleTag["description"]=rulePT
+                                        except Exception:
+                                            print "falha na traducao"
+                                            sys.exc_clear()
+
+                                    else:
+                                        print "Nao existe no dicionario", rule
+
+                                for characteristicsTag in catElement.find_all("characteristic"):
+                                    if characteristicsTag["name"] in ["Abilities", "Description"]:
+                                        # print profiles["name"],   characteristic["name"], characteristic["value"],characteristic
+
+                                        #print characteristicsTag
+
+                                        if characteristicsTag.has_attr('value'):
+                                            description = characteristicsTag["value"]
+                                            #print description
+                                            if description in dicionario.keys() and len(description) > 1:
+                                                descriptionPT =dicionario[description]
+                                                # print "adicionando", description
+                                                print "Traduzindo>>>\n", description, "\n", descriptionPT
+                                                #print type(cat)
+                                                try:
+                                                    #cat.find(text=description).replaceWith(descriptionPT)
+
+                                                    characteristicsTag["value"]=descriptionPT
+                                                except Exception:
+                                                    print "falha na traducao"
+                                                    sys.exc_clear()
+                                            else:
+                                                print "nao existe no dicionario", description
+
+
+        # TRADUS ARQUIVO GST
+        for catalogues in cat.find_all("gameSystem"):
+            for catalogue in catalogues.children:
+                # print catalogue.name, type(catalogue)
+                if catalogue.name <> None:
+                    print 'n1', catalogue.name
+                    if catalogue.name in ["sharedrules", "sharedprofiles", "selectionentries", "sharedselectionentries",
+                                          "sharedselectionentrygroups","sharedRules","sharedProfiles","sharedSelectionEntryGroups",
+                                          "sharedSelectionEntries","selectionEntries",""]:
+                        for catElement in catalogue.children:
+                            if catElement.name <> None:
+                                for ruleTag in catElement.find_all("description"):
+                                    rule = ruleTag.get_text()  #
+                                    # print rule
+                                    if rule in dicionario.keys() and len(rule) > 1:
+                                        rulePT = dicionario[rule]
+                                        print "Traduzindo>>>\n", rule, "\n", rulePT
+                                        #print type(cat)
+                                        try:
+                                            #cat.find(text=rule).replaceWith(rulePT)
+                                            ruleTag["description"]=rulePT
+                                        except Exception:
+                                            print "falha na traducao"
+                                            sys.exc_clear()
+
+                                    else:
+                                        print "Nao existe no dicionario", rule
+
+                                for characteristicsTag in catElement.find_all("characteristic"):
+                                    if characteristicsTag["name"] in ["Abilities", "Description"]:
+                                        # print profiles["name"],   characteristic["name"], characteristic["value"],characteristic
+
+                                        #print characteristicsTag
+
+                                        if characteristicsTag.has_attr('value'):
+                                            description = characteristicsTag["value"]
+                                            #print description
+                                            if description in dicionario.keys() and len(description) > 1:
+                                                descriptionPT =dicionario[description]
+                                                # print "adicionando", description
+                                                print "Traduzindo>>>\n", description, "\n", descriptionPT
+                                                #print type(cat)
+                                                try:
+                                                    #cat.find(text=description).replaceWith(descriptionPT)
+
+                                                    characteristicsTag["value"]=descriptionPT
+                                                except Exception:
+                                                    print "falha na traducao"
+                                                    sys.exc_clear()
+                                            else:
+                                                print "nao existe no dicionario", description
+
+
+
+    print '\n\n\n\n', cfile, 'Tradução Finalizada'
+    #print cat
+    newFile = destPath + cfile
+    xml = cat.prettify("utf-8")
+    with open(newFile, "wb") as file:
+        file.write(xml)
+        file.close()
+
+#compacta
+compactCat(destPath)
--- WarhammerRosterTradutorReadCat.py
+++ WarhammerRosterTradutorReadCat.py
@@ -5,6 +5,7 @@
 
 ########################################################################################################################
 # Le o arquivo cat e traduz com o google tradutor
+# gera o carquivo cat traduzido na pasta wh40k_PTBR-master
 ########################################################################################################################
 
 import sys
@@ -14,25 +15,39 @@ import json
 from bs4 import BeautifulSoup
 from googletrans import Translator
 translator = Translator()
+import os
 
-filesToRead = ["./wh40k-master/Imperium - Space Marines.cat", "./wh40k-master/Imperium - Space Wolves.cat"]
+#filesToRead = ["./wh40k-master/Imperium - Space Marines.cat", "./wh40k-master/Imperium - Space Wolves.cat", "./wh40k-master/Warhammer 40,000 8th Edition.gst"]
+filesToRead = ["Imperium - Space Marines.cat", "Imperium - Space Wolves.cat", "Warhammer 40,000 8th Edition.gst"]
 
-for catFilesToRead in filesToRead:
-    catFile = open(catFilesToRead)
-    cat = BeautifulSoup(catFile, "lxml")
+originPath = "./wh40k-master/"
+destPath = "./wh40k_PTBR-master/"
+
+if not os.path.exists(destPath):
+    os.makedirs(destPath)
+
+catFiles = os.listdir(originPath)
+
+filesToRead = filter(lambda x: x in filesToRead, catFiles)
+
+
+for catFilesToRead  in filesToRead:
+    with open(originPath+catFilesToRead) as catFile:
+        x = catFile.read()
+        catFile.close()
+    cat = BeautifulSoup(x, "xml")
 
     with open('dicionario.json', "r") as file:
-        dicionario =  json.loads(file.read() )
-
-
-    #dicionario={}
+        dicionario = json.loads(file.read() )
 
     for catalogues in cat.find_all("catalogue"):
         for catalogue in catalogues.children:
             #print catalogue.name, type(catalogue)
             if catalogue.name <> None:
                 print 'n1',catalogue.name
-                if catalogue.name in ["sharedrules","sharedprofiles","selectionentries","sharedselectionentries","sharedselectionentrygroups"]:
+                if catalogue.name in ["sharedrules", "sharedprofiles", "selectionentries", "sharedselectionentries",
+                                          "sharedselectionentrygroups","sharedRules","sharedProfiles","sharedSelectionEntryGroups",
+                                          "sharedSelectionEntries","selectionEntries"]:
                     for catElement in catalogue.children:
                         if catElement.name <> None:
                             #print 'n2\t',catElement.name
@@ -52,34 +67,57 @@ for catFilesToRead in filesToRead:
                             for characteristicsTag in catElement.find_all("characteristic"):
                                 if characteristicsTag["name"] in["Abilities", "Description"]:
                                         #print profiles["name"],   characteristic["name"], characteristic["value"],characteristic
-                                        description=characteristicsTag["value"]
-                                        print description
-                                        if description not in dicionario.keys() and len(description) > 1:
-                                            descriptionPT = translator.translate(description, dest='pt').text
-                                            #print "adicionando", description
-                                            print "Traduzindo>>>\n", description, "\n", descriptionPT
-                                            dicionario[description] = descriptionPT
-                                        else:
-                                            print "ja existe no dicionario", description
+                                        if characteristicsTag.has_attr('value'):
+                                            description=characteristicsTag["value"]
+                                            print description
+                                            if description not in dicionario.keys() and len(description) > 1:
+                                                descriptionPT = translator.translate(description, dest='pt').text
+                                                #print "adicionando", description
+                                                print "Traduzindo>>>\n", description, "\n", descriptionPT
+                                                dicionario[description] = descriptionPT
+                                            else:
+                                                print "ja existe no dicionario", description
 
+    for catalogues in cat.find_all("gameSystem"):
+        for catalogue in catalogues.children:
+            #print catalogue.name, type(catalogue)
+            if catalogue.name <> None:
+                print 'n1',catalogue.name
+                if catalogue.name in ["sharedrules", "sharedprofiles", "selectionentries", "sharedselectionentries",
+                                          "sharedselectionentrygroups","sharedRules","sharedProfiles","sharedSelectionEntryGroups",
+                                          "sharedSelectionEntries","selectionEntries"]:
+                    for catElement in catalogue.children:
+                        if catElement.name <> None:
+                            #print 'n2\t',catElement.name
+                            #print '____'
+                            #print "desc",catElement.description
+                            #print '____'
+                            for ruleTag in catElement.find_all("description"):
+                                rule = ruleTag.get_text()#
+                                #print rule
+                                if rule not in dicionario.keys() and len(rule) > 1:
+                                    rulePT = translator.translate(rule, dest='pt').text
+                                    print "Traduzindo>>>\n",rule,"\n",rulePT
+                                    dicionario[rule] = rulePT
+                                else:
+                                    print "ja existe no dicionario", rule
+
+                            for characteristicsTag in catElement.find_all("characteristic"):
+                                if characteristicsTag["name"] in["Abilities", "Description"]:
+                                        #print profiles["name"],   characteristic["name"], characteristic["value"],characteristic
+                                        if characteristicsTag.has_attr('value'):
+                                            description=characteristicsTag["value"]
+                                            print description
+                                            if description not in dicionario.keys() and len(description) > 1:
+                                                descriptionPT = translator.translate(description, dest='pt').text
+                                                #print "adicionando", description
+                                                print "Traduzindo>>>\n", description, "\n", descriptionPT
+                                                dicionario[description] = descriptionPT
+                                            else:
+                                                print "ja existe no dicionario", description
 
 
     newFile = "dicionario.json"
     with open(newFile, "wb") as file:
         file.write(json.dumps(dicionario,indent=4, sort_keys=True, ensure_ascii=False))
 
-    # print 'n3\t\t',catElement.contents[9]
-    # profile= catElement.contents[9]
-    # print type(profile)
-    # for rule in profile.find_all("rule"):
-    #     print rule
-    # for characteristic in profile.find_all("characteristic"):
-    #     if characteristic["name"] in["Abilities", "Description"]:
-    #         #print profiles["name"],   characteristic["name"], characteristic["value"],characteristic
-    #         description=characteristic["value"]
-    #         if description not in dicionario.keys():
-    #
-    #             #print "adicionando", description
-    #             dicionario[description] = ""
-    #         #else:
-    #         #    print "ja existe", description
